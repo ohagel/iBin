c365a0a31196433e297d6a15b9e52a8674a78736
Aske <askepihl@hotmail.com>
2024-01-05 11:50:46 +0100

    blue test

---
+++ esp32/neopixelTest/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 32
+#define NUMPIXELS 28
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+
+
+void setup() {
+  //init with blue color
+  pixels.begin();
+  //set all to blue in for loop
+  for(int i=0; i<NUMPIXELS; i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,255));
+    pixels.show();
+    
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
