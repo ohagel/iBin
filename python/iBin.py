997a2f498b91af5280d88194f9158807436091d0
Oliver Hagel <mr.oliver.hagel@gmail.com>
2024-01-17 16:00:36 +0100

    Add FPS calculation to iBin.py

--- python/iBin.py
+++ python/iBin.py
@@ -17,7 +17,9 @@ if __name__ == '__main__':
     net.load('iBin_net2_16-01-2024_19;40.pth')
 
     #main inference loop
+    startTime = 0
     while True:
+        endTime = time.time()
         frame = iBin.getFrame()
         shownFrame = frame 
         if not iBin.getLid():
@@ -26,13 +28,16 @@ if __name__ == '__main__':
             res = net.infer(frame , weight)
             cv2.putText(shownFrame, "Class: " + str(classes[res]), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
             cv2.putText(shownFrame, "Weight: " + str(weight), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(shownFrame, "FPS: " + "{:.2f}".format(1/(endTime-startTime)), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         else:
             iBin.setLight(0)
             cv2.putText(shownFrame, "Lid open", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(shownFrame, "FPS: " + "{:.2f}".format(1/(endTime-startTime)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         cv2.imshow('Live view', shownFrame)
-        key = cv2.waitKey(20)
+        key = cv2.waitKey(1)
         if key == ord('q'):
             break
+        startTime = endTime
 
     iBin.close()
     iBin.cap.release()
