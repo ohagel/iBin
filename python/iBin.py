46fbb62cf7ef9f47bcf089fe34ad900fab059303
Oliver Hagel <mr.oliver.hagel@gmail.com>
2024-01-18 10:56:02 +0100

    Update class names and add text overlay

--- python/iBin.py
+++ python/iBin.py
@@ -2,6 +2,7 @@ from iBinNet2 import Net
 import cv2
 from iBinCom import iBinCom
 import time
+import numpy as np
 
 if __name__ == '__main__':
     #init iBin communication
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     time.sleep(1) #small delay to let the microcontroller boot
 
     #defining classes
-    classes = ('plastic', 'cardboard', 'metal', 'glass') 
+    classes = ('Plastic', 'Cardboard', 'Metal', 'Glass') 
     #init neural network 
     net = Net(device='cuda:0')
     #Load trained model
@@ -22,17 +23,19 @@ if __name__ == '__main__':
         endTime = time.time()
         frame = iBin.getFrame()
         shownFrame = frame 
+        textFrame = np.zeros((frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
         if not iBin.getLid():
             iBin.setLight(255)
             weight = iBin.getWeight()
             res = net.infer(frame , weight)
-            cv2.putText(shownFrame, "Class: " + str(classes[res]), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.putText(shownFrame, "Weight: " + str(weight), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.putText(shownFrame, "FPS: " + "{:.2f}".format(1/(endTime-startTime)), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(textFrame, "Class: " + str(classes[res]), (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)#, cv2.LINE_AA)
+            cv2.putText(textFrame, "Weight: " + str(weight), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)#, cv2.LINE_AA)
+            cv2.putText(textFrame, "FPS: " + "{:.2f}".format(1/(endTime-startTime)), (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)#, cv2.LINE_AA)
         else:
             iBin.setLight(0)
-            cv2.putText(shownFrame, "Lid open", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.putText(shownFrame, "FPS: " + "{:.2f}".format(1/(endTime-startTime)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(textFrame, "Lid open", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)#, cv2.LINE_AA)
+            cv2.putText(textFrame, "FPS: " + "{:.2f}".format(1/(endTime-startTime)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)#, cv2.LINE_AA)
+        shownFrame[textFrame[:,:,0] > 0] = 255 - shownFrame[textFrame[:,:,0] > 0]
         cv2.imshow('Live view', shownFrame)
         key = cv2.waitKey(1)
         if key == ord('q'):
